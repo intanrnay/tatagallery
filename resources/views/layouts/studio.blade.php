36e14a6f29d942a4445ac3590c9660f72c091ca3
intanrnay <intanrayna14@gmail.com>
2024-04-01 05:33:36 +0700

    css

--- resources/views/layouts/studio.blade.php
+++ resources/views/layouts/studio.blade.php
@@ -2,13 +2,19 @@
 
 @section('content')
 
-<div >
-<a type="button" class="post" href="/createfoto" style="float : left;">
-  <span>Create New Post</span>
-</a>
+<div class="dropdown-1">
+  <button class="dropbtn">Create New</button>
+  <div class="dropdown-content-1">
+    <a href="/createalbum">Create New Album</a>
+    <a href="/createfoto">Create New Foto</a>
+  </div>
+</div>
 
-<a type="button" class="post" href="/createalbum" >
-  <span>Create New Album</span>
-</a>
+<div class="col-md-4">
+  @foreach ($fotos as $foto)
+    <div class="display-foto"><a href="">
+      <img src="{{ asset('storage/foto/'.$foto->lokasi_file) }}"></a></div>
+  @endforeach
+
 </div>
 @endsection
--- routes/web.php
+++ routes/web.php
@@ -22,27 +22,10 @@ use App\Http\Controllers\SignupController;
 |
 */
 
-Route::get('/', function () {
-    return view('main', [
-        "title" => "Home"
-    ]);
-});
 
 Route::get('/studio', function () {
     return view('layouts.studio', [
         "title" => "Studio"
-    ]);
-});
-
-Route::get('/sign-in', function () {
-    return view('layouts.sign-in', [
-        "title" => "Sign-In"
-    ]);
-});
-
-Route::get('/sign-up', function () {
-    return view('layouts.sign-up', [
-        "title" => "Sign-Up"
     ]);
 });
 
@@ -54,11 +37,11 @@ Route::get('/sign-up', [SignupController::class, 'index'])->middleware('guest');
 Route::post('/sign-up', [SignupController::class, 'store']);
 Route::post('/logout', [SigninController::class, 'logout']);
 
-Route::get('/', [HomeController::class, 'index'])->middleware('auth');
+Route::get('/', [HomeController::class, 'index']);
 
-Route::get ('/createfoto', [FotoController::class, 'index']);
+Route::get ('/createfoto', [FotoController::class, 'create']);
 Route::post('/upload/photo', [FotoController::class, 'upload'])->name('upload.photo');
 
-Route::get('/createalbum', [AlbumController::class, 'index']);
+Route::get('/createalbum', [AlbumController::class, 'create']);
 Route::post('/album/new', [AlbumController::class, 'store'])->name('album.new');
 // Route::get('/creating', [PostController::class, 'index']);
